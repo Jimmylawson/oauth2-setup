b6f3c0e09b0f671a1650c3650c838ef3d4bae0de
jimmy <jimoseipeace2bb@gmail.com>
2026-04-10 14:47:41 -0500

    change applicastion properties to yml

--- src/main/java/com/example/additioncalculator/SecurityChain.java
+++ src/main/java/com/example/additioncalculator/SecurityChain.java
@@ -3,14 +3,37 @@ package com.example.additioncalculator;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.OAuth2ResourceServerDsl;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityChain {
 
+    private String keySetU
     @Bean
+    @Order(2)
     public SecurityFilterChain filterChain(HttpSecurity http){
+        http
 
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(s->s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth->{
+                    auth
+                            .requestMatchers(
+                                    HttpMethod.POST,"/api/calculator/").authenticated()
+                            .requestMatchers(HttpMethod.GET, "/api/calculator/").authenticated()
+                            .anyRequest().authenticated();
+
+                });
+
+        http.formLogin(AbstractHttpConfigurer::disable);
+        http.httpBasic(AbstractHttpConfigurer::disable);
+        return http.build();
     }
 }
